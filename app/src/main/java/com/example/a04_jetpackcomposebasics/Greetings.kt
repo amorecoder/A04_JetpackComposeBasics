fd680549f4761c1bee3ec709fa5d417ca3158402
batspike <samlau.sam@gmail.com>
2022-06-27 19:48:29 +0800

    04 - State hoisting

---
+++ app/src/main/java/com/example/a04_jetpackcomposebasics/Greetings.kt
@@ -0,0 +1,49 @@
+package com.example.a04_jetpackcomposebasics
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.OutlinedButton
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.*
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun Greetings(names: List<String> = listOf("World", "Compose")) {
+    Column(modifier = Modifier.padding(vertical = 4.dp)) {
+        for(name in names) {
+            Greeting(name = name)
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    var expanded by remember { mutableStateOf(false) }
+    val extraPadding = if(expanded) 48.dp else 0.dp // no need for remember since we already depends on another remember-ed state.
+
+    Surface(
+        color = MaterialTheme.colors.primary,
+        modifier = Modifier.padding(vertical = 4.dp, horizontal = 8.dp)
+    ) {
+        Row(modifier = Modifier.padding(24.dp)) {
+            // the weight modifier will push the button to the right
+            Column(
+                modifier = Modifier
+                    .weight(1f)
+                    .padding(bottom = extraPadding)
+            ) {
+                Text(text = "Hello,")
+                Text(text = name)
+            }
+            OutlinedButton(
+                onClick = { expanded = !expanded }
+            ) {
+                Text(text = if(expanded) "Show less" else "Show more")
+            }
+        }
+    }
+}
