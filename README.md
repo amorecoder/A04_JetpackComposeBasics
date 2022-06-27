570f7d86d9678f9d191f161f146e7d8d8c8505db
batspike <samlau.sam@gmail.com>
2022-06-27 15:27:06 +0800

    02 - Rows and Columns

--- README.md
+++ README.md
@@ -15,5 +15,6 @@ In this codelab, we will learn:
 2. We use setContent to define our layout, but instead of using an XML file as per traditional view definition, we call Composable functions.
 3. The Surface composable understands that when background is set to primary color, any text on top of it should use the onPrimary color which is defined in the theme.
 4. Modifiers tell a UI element how to lay out, display, or behave within its parent layout. Complete list of modifiers are documented in https://developer.android.com/jetpack/compose/modifiers-list .
-5. 
-6. 
+5. The three basic standard layout elements in Compose are Column, Row, and Box.
+6. The weight modifier makes the element fill all available space, making it flexible, effectively pushing away the other elements that don't have a weight, which are called inflexible. It also makes the fillMaxWidth modifier redundant.
+7. 
--- app/src/main/java/com/example/a04_jetpackcomposebasics/MainActivity.kt
+++ app/src/main/java/com/example/a04_jetpackcomposebasics/MainActivity.kt
@@ -3,13 +3,11 @@ package com.example.a04_jetpackcomposebasics
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.a04_jetpackcomposebasics.ui.theme.A04_JetpackComposeBasicsTheme
@@ -19,14 +17,17 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             A04_JetpackComposeBasicsTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Greeting("Android")
-                }
+                MyApp()
             }
+        }
+    }
+}
+
+@Composable
+private fun MyApp(names: List<String> = listOf("World", "Compose")) {
+    Column(modifier = Modifier.padding(vertical = 4.dp)) {
+        for(name in names) {
+            Greeting(name = name)
         }
     }
 }
@@ -34,15 +35,30 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun Greeting(name: String) {
     // Surface allows us to set a different background color
-    Surface(color = MaterialTheme.colors.primary) {
-        Text(text = "Hello $name!", modifier = Modifier.padding(24.dp))
+    Surface(
+        color = MaterialTheme.colors.primary,
+        modifier = Modifier.padding(vertical = 4.dp, horizontal = 8.dp)
+    ) {
+        Row(modifier = Modifier.padding(24.dp)) {
+            // the weight modifier will push the button to the right
+            Column( modifier = Modifier.weight(1f) ) {
+                Text(text = "Hello,")
+                Text(text = name)
+            }
+            OutlinedButton(
+                modifier = Modifier.shadow(elevation = 4.dp),
+                onClick = {  }
+            ) {
+                Text(text = "Show more")
+            }
+        }
     }
 }
 
-@Preview(showBackground = true)
+@Preview(showBackground = true, widthDp = 320)
 @Composable
 fun DefaultPreview() {
     A04_JetpackComposeBasicsTheme {
-        Greeting("Android")
+        MyApp()
     }
 }
